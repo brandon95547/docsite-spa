d032df4419fba408608121681bd53434a887efb4
Brandon Sanders <brandon95547@gmail.com>
2019-06-15 23:31:26 -0400

    adding scrollspy support with pure javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,58 +2,82 @@ initSmoothScrolling();
 
 function initSmoothScrolling() {
 
-    
+
     var duration = 400;
-    
-    var pageUrl = location.hash
-        ? stripHash(location.href)
-        : location.href
-    ;
-    
+
+    var pageUrl = location.hash ?
+        stripHash(location.href) :
+        location.href;
+
     delegatedLinkHijacking();
 
     var sideNavItems = document.querySelectorAll('.list-s1 li a');
     var sideNav = document.querySelector('.chapters>ul');
     var sideNavToggle = document.querySelector('.list-s1__toggle');
+    
+    // set links active when scrolling into view
+    window.addEventListener('scroll', function (e) {
+        
+        var formsTop = document.querySelector('#forms').getBoundingClientRect().top - 117;
+        var advertsTop = document.querySelector('#adverts').getBoundingClientRect().top - 117;
+        
+        [].forEach.call(sideNavItems, function (a) {
+            var area = a.getAttribute('href');
+            var top = document.querySelector(area).getBoundingClientRect().top - 117;
+            if(top > -175 && top < 175) {
+                clearActiveStates();
+                if(!a.classList.contains('active')) {
+                    a.classList.add('active');
+                }
+            }
+            console.log(top);
+        });
 
-    sideNavToggle.addEventListener('click', function(e) {
-        if(!e.target.classList.contains('open')) {
+        
+    });
+
+
+    sideNavToggle.addEventListener('click', function (e) {
+        if (!e.target.classList.contains('open')) {
             e.target.classList.add('open');
             sideNav.classList.add('open');
-        }
-        else {
+        } else {
             e.target.classList.remove('open');
             sideNav.classList.remove('open');
         }
     }, false);
-    
+
+    function clearActiveStates() {
+        [].forEach.call(sideNavItems, function (a) {
+            a.classList.remove('active');
+        });
+    }
+
     function delegatedLinkHijacking() {
         document.body.addEventListener('click', onClick, false);
-        
+
         function onClick(e) {
             if (!isInPageLink(e.target))
                 return;
-            
+
             e.stopPropagation();
             e.preventDefault();
 
-            [].forEach.call(sideNavItems, function(a) {
-                a.classList.remove('active');
-            });
+            clearActiveStates();
 
             e.target.classList.add('active');
 
             // when using mobile, toggle the open class and adjust the offset for the scroll
-            if(sideNavToggle.classList.contains('open')) {
+            if (sideNavToggle.classList.contains('open')) {
                 sideNavToggle.classList.remove('open');
                 sideNav.classList.remove('open');
                 var offset = -25;
             }
-            
+
             jump(e.target.hash, {
                 duration: duration,
                 offset: typeof offset != 'undefined' ? offset : 0,
-                callback: function() {
+                callback: function () {
                     setFocus(e.target.hash);
                 }
             });
@@ -61,12 +85,11 @@ function initSmoothScrolling() {
     }
 
     function isInPageLink(n) {
-        return n.tagName.toLowerCase() === 'a' 
-            && n.hash.length > 0
-            && stripHash(n.href) === pageUrl
-        ;
+        return n.tagName.toLowerCase() === 'a' &&
+            n.hash.length > 0 &&
+            stripHash(n.href) === pageUrl;
     }
-        
+
     function stripHash(url) {
         return url.slice(0, url.lastIndexOf('#'));
     }
